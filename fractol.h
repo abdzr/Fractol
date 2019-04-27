2cef1079d8cef5831cc2a327197a2f7274129c0e
Abdelilah Zarzor <azarzor@e1r2p4.1337.ma>
2019-04-27 16:24:15 +0100

    updated fractol header and used while loops

--- fractol.h
+++ fractol.h
@@ -6,32 +6,37 @@
 /*   By: azarzor <azarzor@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/21 15:30:14 by azarzor           #+#    #+#             */
-/*   Updated: 2019/04/26 19:24:01 by azarzor          ###   ########.fr       */
+/*   Updated: 2019/04/27 16:13:56 by azarzor          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
 #ifndef FRACTOL_H
-# define FRACTOL_H
+#define FRACTOL_H
 
-# include <mlx.h>
-# include <unistd.h>
-# include <stdio.h>
-# include <stdlib.h>
-# include <string.h>
-
+#include <mlx.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 typedef struct s_env t_env;
-struct  s_env
+struct s_env
 {
-    void    *mlx_ptr;
-    void    *mlx_win;
-    void    *mlx_img;
-    int     *mlx_data;
-    int     bpp;
-    int     endian;
-    int     size_l;
-    char    clr[4];
+    int         row;
+    int         col;
+    double      cre;
+    double      cim;
+    double      x;
+    double      y;
+    double      xnew;
+    int         iter;
+    int         *mlx_data;
+    int         bpp;
+    int         endian;
+    int         size_l;
+    void        *mlx_ptr;
+    void        *mlx_win;
+    void        *mlx_img;
 };
 
 #endif
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: azarzor <azarzor@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/27 13:26:56 by azarzor           #+#    #+#             */
-/*   Updated: 2019/04/27 14:24:11 by azarzor          ###   ########.fr       */
+/*   Updated: 2019/04/27 16:23:26 by azarzor          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,44 +14,48 @@
 
 int rgb(int r, int g, int b)
 {
-    char clr[4];
-    clr[0] = b;
-    clr[1] = g;
-    clr[2] = r;
-    clr[3] = 1;
-    return (*(int *)clr);
+	char clr[4];
+	clr[0] = b;
+	clr[1] = g;
+	clr[2] = r;
+	clr[3] = 1;
+	return (*(int *)clr);
 }
 
 int main()
 {
-    t_env *env;
+	t_env *env;
 
-    env = (t_env *)malloc(sizeof(t_env));
-    env->mlx_ptr = mlx_init();
-    env->mlx_win = mlx_new_window(env->mlx_ptr, 600, 600, "testing");
-    env->mlx_img = mlx_new_image(env->mlx_ptr, 600, 600);
-    env->mlx_data = (int *)mlx_get_data_addr(env->mlx_img, &env->bpp, &env->size_l, &env->endian);
+	env = (t_env *)malloc(sizeof(t_env));
+	env->mlx_ptr = mlx_init();
+	env->mlx_win = mlx_new_window(env->mlx_ptr, 600, 600, "testing");
+	env->mlx_img = mlx_new_image(env->mlx_ptr, 600, 600);
+	env->mlx_data = (int *)mlx_get_data_addr(env->mlx_img, &env->bpp, &env->size_l, &env->endian);
+	env->row = -1;
+	while (++env->row < 600)
+	{
+		env->col = -1;
+		while (++env->col < 600)
+		{
+			// must be optimised.
+			env->cre = (env->col - 600 / 2.0) * 4.0 / 600;
+			env->cim = (env->row - 600 / 2.0) * 4.0 / 600;
 
-    for (int row = 0; row < 600; row++)
-    {
-        for (int col = 0; col < 600; col++)
-        {
-            double c_re = (col - 600 / 2.0) * 4.0 / 600;
-            double c_im = (row - 600 / 2.0) * 4.0 / 600;
-            double x = 0, y = 0; 
-            int iteration = 0;
-            while (x * x + y * y <= 4 && iteration < 30)
-            {
-                double x_new = x * x - y * y + c_re;
-                y = 2 * x * y + c_im;
-                x = x_new;
-                iteration++;
-            }
-            if (iteration < 30)
-            env->mlx_data[row * 600 + col] = rgb(255, 0, 0);
-        }
-    }
-    mlx_put_image_to_window(env->mlx_ptr, env->mlx_win, env->mlx_img, 0, 0);
-    mlx_loop(env->mlx_ptr);
-    return (0);
+			env->x = 0;
+			env->y = 0;
+			env->iter = 0;
+			while (env->x * env->x + env->y * env->y <= 4 && env->iter < 30)
+			{
+				env->xnew = env->x * env->x - env->y * env->y + env->cre;
+				env->y = 2 * env->x * env->y + env->cim;
+				env->x = env->xnew;
+				env->iter++;
+			}
+			if (env->iter < 30)
+				env->mlx_data[env->row * 600 + env->col] = rgb(255, 255, 255);
+		}
+	}
+	mlx_put_image_to_window(env->mlx_ptr, env->mlx_win, env->mlx_img, 0, 0);
+	mlx_loop(env->mlx_ptr);
+	return (0);
 }
