afc7b2e13acdb8385b0eac78641737dd057c06da
Abdelilah Zarzor <azarzor@e1r2p10.1337.ma>
2019-05-18 03:55:46 +0000

    added Julia v3 and v4 and Mandelbrot v3 and v4

--- mandelbrot.c
+++ mandelbrot.c
@@ -6,7 +6,7 @@
 /*   By: azarzor <azarzor@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/30 22:53:04 by azarzor           #+#    #+#             */
-/*   Updated: 2019/05/16 13:06:12 by azarzor          ###   ########.fr       */
+/*   Updated: 2019/05/18 03:55:10 by azarzor          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,6 +41,64 @@ void		mandeldraw(t_env *env)
 			{
 				env->xnew = env->x * env->x - env->y * env->y + (env->cre);
 				env->y = (2 * env->x * env->y + (env->cim));
+				env->x = env->xnew;
+				env->iter++;
+			}
+			if (env->iter < env->max)
+				env->mlx_data[env->row * WIN_W + env->col] =
+					env->colors[env->iter % 24];
+		}
+	}
+	mlx_put_image_to_window(env->mlx_ptr, env->mlx_win, env->mlx_img, 0, 0);
+}
+
+void		mandeldrawv3(t_env *env)
+{
+	images(env);
+	while (++env->row < WIN_W)
+	{
+		env->col = -1;
+		while (++env->col < WIN_H)
+		{
+			env->cre = env->mnre + ((env->mxre - env->mnre) / WIN_W) * env->col;
+			env->cim = env->mnim + ((env->mxim - env->mnim) / WIN_H) * env->row;
+			env->x = 0;
+			env->y = 0;
+			env->iter = 0;
+			while (env->x * env->x + env->y * env->y <= 4
+					&& env->iter < env->max)
+			{
+				env->xnew = env->x * env->x * env->x - 3 * env->x * env->y * env->y + (env->cre);
+				env->y = 3 * env->x * env->x * env->y - env->y * env->y * env->y + (env->cim);
+				env->x = env->xnew;
+				env->iter++;
+			}
+			if (env->iter < env->max)
+				env->mlx_data[env->row * WIN_W + env->col] =
+					env->colors[env->iter % 24];
+		}
+	}
+	mlx_put_image_to_window(env->mlx_ptr, env->mlx_win, env->mlx_img, 0, 0);
+}
+
+void		mandeldrawv4(t_env *env)
+{
+	images(env);
+	while (++env->row < WIN_W)
+	{
+		env->col = -1;
+		while (++env->col < WIN_H)
+		{
+			env->cre = env->mnre + ((env->mxre - env->mnre) / WIN_W) * env->col;
+			env->cim = env->mnim + ((env->mxim - env->mnim) / WIN_H) * env->row;
+			env->x = 0;
+			env->y = 0;
+			env->iter = 0;
+			while (env->x * env->x + env->y * env->y <= 4
+					&& env->iter < env->max)
+			{
+				env->xnew =env->x * env->x * env->x * env->x - 6 * env->x * env->x * env->y * env->y + env->y * env->y * env->y * env->y + (env->cre);
+				env->y =4 * env->x * env->x * env->x * env->y - 4 * env->x * env->y * env->y * env->y + (env->cim);
 				env->x = env->xnew;
 				env->iter++;
 			}
