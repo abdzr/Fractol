8864a6a55713c7dd55ce80bf1ba6043fd560d324
Abdelilah Zarzor <azarzor@e1r2p7.1337.ma>
2019-06-04 08:43:58 +0000

    julia v3 works with multithreading

--- Julia.c
+++ Julia.c
@@ -6,7 +6,7 @@
 /*   By: azarzor <azarzor@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/12 15:43:37 by azarzor           #+#    #+#             */
-/*   Updated: 2019/06/04 04:10:17 by azarzor          ###   ########.fr       */
+/*   Updated: 2019/06/04 08:39:32 by azarzor          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -177,33 +177,6 @@ void juliadraw(t_env *env)
 
 
 /* EVERYTHING ELSE */
-void juliadrawv3(t_env *env)
-{
-	images(env);
-	while (++env->row < WIN_W)
-	{
-		env->col = -1;
-		while (++env->col < WIN_H)
-		{
-			env->cre = env->mnre + ((env->mxre - env->mnre) / WIN_W) * env->col;
-			env->cim = env->mnim + ((env->mxim - env->mnim) / WIN_H) * env->row;
-			env->x = env->cre;
-			env->y = env->cim;
-			env->iter = 0;
-			while (env->x * env->x + env->y * env->y <= 4 && env->iter < env->max)
-			{
-				env->xnew = env->x * env->x * env->x - 3 * env->x * env->y * env->y + env->jul_cre;
-				env->y = 3 * env->x * env->x * env->y - env->y * env->y * env->y + env->jul_cim;
-				env->x = env->xnew;
-				env->iter++;
-			}
-			if (env->iter < env->max)
-				env->mlx_data[env->row * WIN_W + env->col] =
-					env->colors[env->iter % 24];
-		}
-	}
-	mlx_put_image_to_window(env->mlx_ptr, env->mlx_win, env->mlx_img, 0, 0);
-}
 
 void juliadrawv4(t_env *env)
 {
